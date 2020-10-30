7ed92f957d848f5af7318bd6fd04b97e2c93c139
npinkam <n.pinkam@lexer.co.jp>
2020-10-30 17:36:50 +0900

    initial commit

---
+++ node-generator.js
@@ -0,0 +1,28 @@
+module.exports = function(RED) {
+    function nodeGeneratorNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        //properties field
+        this.name = config.name;
+        this.desc = config.desc;
+        this.properties = config.properties;
+        this.codeBeforeReceivePayload = config.codeBeforeReceivePayload;
+        this.codeWhenReceivePayload = config.codeWhenReceivePayload;
+
+        node.on('input', function(msg) {
+            msg.payload = {};
+            msg.payload.name = this.name;
+            msg.filename = '/data/node-creation/lexer-nodered-' + this.name +'/';
+            msg.payload.desc = this.desc;
+            if(this.properties)
+                msg.payload.properties = this.properties.split(',');
+            else
+                msg.payload.properties = {};
+            msg.payload.codeBeforeReceivePayload = this.codeBeforeReceivePayload;
+            msg.payload.codeWhenReceivePayload = this.codeWhenReceivePayload;
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("node-generator",nodeGeneratorNode);
+}
